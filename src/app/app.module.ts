0134c0e8a6e9347bd22b351e22ad5669f5957138
ZippMukami <zimukami@gmail.com>
2022-04-20 00:26:55 +0300

    adds services and components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { SearchComponent } from './component/search/search.component';
+import { UserDetailsComponent } from './component/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
