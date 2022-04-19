c13792dda42433dfce2043f6ef880e7317bafec8
ZippMukami <zimukami@gmail.com>
2022-04-20 02:39:35 +0300

    adds route activation.

--- src/app/component/user-details/user-details.component.ts
+++ src/app/component/user-details/user-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private active : ActivatedRoute) { }
 
   ngOnInit(): void {
   }
