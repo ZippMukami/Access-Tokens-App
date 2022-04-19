b7b2ee01c5540c36df1e4333097f304f3240dbef
ZippMukami <zimukami@gmail.com>
2022-04-20 00:42:23 +0300

    adds the get user function.

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -7,6 +7,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GithubService {
+  url : string ="https://api.github.com/users/"
 
   constructor(private http: HttpClient) { }
+
+
+  getUser( username :string){
+    //https://api.github.com/users/username
+
+
+    this.http.get(this.url + username)
+  }
 }
+
